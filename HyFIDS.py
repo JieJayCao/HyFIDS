b6dc9a36c05ee99d31ff5aa5f0302f826d44cef3
Jerrick <45455827+JieJayCao@users.noreply.github.com>
2025-05-29 00:10:58 -0400

    Update HyFIDS.py

--- HyFIDS.py
+++ HyFIDS.py
@@ -30,11 +30,11 @@ config = {
     'd_dim': 36,
     'hidden_size': 50, 
     'save_name': 'HyL-{epoch:02d}-{val_acc:.2f}',
-    'log_path1': 'HyL_logs',  # 修改
-    'log_path2': 'Mix_advTrain',  # 修改
-    'num_class': 6 # 修改
+    'log_path1': 'HyL_logs', 
+    'log_path2': 'Mix_advTrain', 
+    'num_class': 6 
 }
-isIat = False  # 修改
+isIat = False 
 
 
 
@@ -62,7 +62,7 @@ class MyDataset(Dataset):
             # label = np.load(path + mode + '.npy')
             if data_label.shape[0] < config['flow_len']:
                 data = data_label[:, :10]
-                pad_width = ((0, 0), (0, config['flow_len'] - 10))  # 在第二个维度上填充40列0
+                pad_width = ((0, 0), (0, config['flow_len'] - 10))  
                 data = np.pad(data, pad_width, mode='constant', constant_values=0)
                 label = data_label[:, -1]
             else:
@@ -74,7 +74,7 @@ class MyDataset(Dataset):
         self.label = torch.LongTensor(label.astype(np.uint8))
 
     def __len__(self):
-        return len(self.data)  # 返回数据的总个数
+        return len(self.data) 
 
     def __getitem__(self, index):
         return self.data[index], self.label[index]
@@ -138,103 +138,3 @@ class HyLIDS(pl.LightningModule):
         return out
     
     
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001) #weight_decay=0.01)
-        #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config['n_epochs'], eta_min=1e-6)
-        return {"optimizer": optimizer}#, "lr_scheduler": scheduler}
-       
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self(x)
-        loss = F.cross_entropy(preds, y)
-        self.log('training_loss', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
-        loss = {'loss': loss}
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self(x)
-        entropy = F.cross_entropy(preds, y)
-        self.log('val_loss', entropy, prog_bar=True, logger=True, on_step=False, on_epoch=True)
-
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self(x)
-        y_pre = torch.argmax(F.log_softmax(preds, dim=1), dim=1)
-        
-        # 获取概率值用于ROC曲线
-        probs = F.softmax(preds, dim=1)[:, 1].cpu().numpy()
-        y_true = y.cpu().numpy()
-        y_pred = y_pre.cpu().numpy()
-        
-        acc = accuracy_score(y_true, y_pred)
-        pre = precision_score(y_true, y_pred, average='macro')
-        recall = recall_score(y_true, y_pred, average='macro')
-        f1 = f1_score(y_true, y_pred, average='macro')
-        
-
-        # 记录所有指标
-        self.log('test_acc', acc)
-        self.log('test_pre', pre)
-        self.log('test_recall', recall)
-        self.log('test_f1', f1)
-        
-    
-
-checkpoint_callback = ModelCheckpoint(
-    monitor='val_loss',
-    filename=config['save_name'],
-    save_top_k=1,
-    mode='min',
-    save_last=True
-)
-
-
-    
-if __name__ == '__main__':
-    tr_set = prep_dataloader(tr_path, 'train', config['batch_size'], njobs=6)
-    val_set = prep_dataloader(val_path, 'val', config['batch_size'], njobs=6)
-    test_set = prep_dataloader(test_path, 'test', config['batch_size'], njobs=1)
-
-    model =  HyLIDS().to(device)
-    
-    wandb_logger = WandbLogger(project="FFT-model-analysis", name="FFT-timing", log_model=True)
-
-    wandb_logger.experiment.config.update({
-        "model_type": "HyLIDS",
-        "dataset": "cic-iot",
-        "epochs": config['n_epochs'],
-        "batch_size": config['batch_size'],
-        "flow_length": config['flow_len'],
-        "embedding_dim": config['d_dim'],
-        "hidden_size": config['hidden_size'],
-        "num_classes": config['num_class'],
-        "device": device
-    })
-    
-    trainer = Trainer(
-    val_check_interval=1.0,
-    max_epochs=config['n_epochs'],
-    accelerator='cpu',      # 👈 使用 CPU
-    logger=wandb_logger,
-    deterministic=True,     # 禁用非确定性优化
-    callbacks=[
-        checkpoint_callback
-    ])
-    wandb_logger.watch(model, log="all")
-
-    start_time = time.time()
-    trainer.fit(model, train_dataloaders=tr_set, val_dataloaders=val_set)
-    # trainer.fit(model, train_dataloaders=tr_set, ckpt_path=r'D:\project\FS-NET_my\model\pktl_v2ray_all.ckpt')
-    end_time = time.time()
-    training_time = end_time - start_time
-    print(f"训练总时间: {training_time:.2f} 秒")
-    print(f"每轮平均时间: {training_time / config['n_epochs']:.2f} 秒")
-    
-    trainer.test(model, dataloaders=test_set)
-    # 保存模型为pt格式
-    torch.save(model.state_dict(), "/home/jie/program/cic-iot/data/hylids_model.pt")
-    print("模型已保存为pt格式：/home/jie/program/cic-iot/data/hylids_model.pt")
-    # # save model
-    #trainer.save_checkpoint("/home/jie/Program/AdvTrafFeat/SimuModel/saved_dict/sub_VPN.ckpt")
